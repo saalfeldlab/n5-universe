d2de72cde38b91cd2e39704405a4a69d4ff881d5
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2021-03-22 21:51:43 -0400

    comment Singleton

--- src/main/java/org/janelia/saalfeldlab/n5/universe/Singleton.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/Singleton.java
@@ -17,11 +17,13 @@ import net.imglib2.type.NativeType;
  *
  * Useful for sharing instances of objects between threads.
  *
- * Our use case for this is to share cached N5 images between
- * threads of a Spark cluster running on the same machine.
+ * Our most relevant use case for this is to share cached ImgLib2 cell images
+ * such as N5 datasets and lazily generated cell images between tasks of a
+ * Spark cluster running on the same executor.
  *
  * Example:
  * <pre>
+ *
  * </pre>
  *
  * @author Stephan Saalfeld
@@ -39,15 +41,32 @@ public class Singleton {
 
 	static private HashMap<String, Object> singletons = new HashMap<>();
 
+	/**
+	 * Remove and retrieve a named singleton instance.
+	 *
+	 * @param key
+	 * @return
+	 */
 	public static synchronized Object remove(final String key) {
 
 		return singletons.remove(key);
 	}
 
+	/**
+	 * Get or create the named singleton instance of T.
+	 *
+	 * @param <T>
+	 * @param <E>
+	 * @param key
+	 * @param supplier
+	 * @return
+	 * @throws E
+	 */
 	public static synchronized <T, E extends Exception> T get(
 			final String key,
 			final ThrowingSupplier<T, E> supplier) throws E {
 
+		@SuppressWarnings("unchecked")
 		final T t = (T)singletons.get(key);
 		if (t == null) {
 			final T s = supplier.get();
@@ -56,6 +75,10 @@ public class Singleton {
 		} else return t;
 	}
 
+
+	/**
+	 * Clear all named singletons.
+	 */
 	public static synchronized void clear() {
 
 		singletons.clear();
