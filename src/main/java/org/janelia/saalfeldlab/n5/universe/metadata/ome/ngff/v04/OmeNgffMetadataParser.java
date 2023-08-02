e0a467e59b83f1c1d6096ad84cb14126687a2803
John Bogovic <bogovicj@janelia.hhmi.org>
2023-08-02 16:36:51 -0400

    chore: ome-zarr v0.4 parser formatting

--- src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/v04/OmeNgffMetadataParser.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/v04/OmeNgffMetadataParser.java
@@ -13,20 +13,17 @@ import org.janelia.saalfeldlab.n5.universe.metadata.N5DatasetMetadata;
 import org.janelia.saalfeldlab.n5.universe.metadata.N5MetadataParser;
 import org.janelia.saalfeldlab.n5.universe.metadata.N5MetadataWriter;
 import org.janelia.saalfeldlab.n5.universe.metadata.N5SingleScaleMetadata;
-import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.OmeNgffMultiScaleMetadata.OmeNgffDataset;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.coordinateTransformations.CoordinateTransformation;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.coordinateTransformations.CoordinateTransformationAdapter;
-import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.coordinateTransformations.TransformationUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 
-import net.imglib2.realtransform.AffineTransform3D;
 
 public class OmeNgffMetadataParser implements N5MetadataParser<OmeNgffMetadata>, N5MetadataWriter<OmeNgffMetadata> {
 
-	private Gson gson;
+	private final Gson gson;
 
 	public OmeNgffMetadataParser() {
 
@@ -40,13 +37,13 @@ public class OmeNgffMetadataParser implements N5MetadataParser<OmeNgffMetadata>,
 	}
 
 	@Override
-	public Optional<OmeNgffMetadata> parseMetadata(N5Reader n5, N5TreeNode node) {
+	public Optional<OmeNgffMetadata> parseMetadata(final N5Reader n5, final N5TreeNode node) {
 
 		OmeNgffMultiScaleMetadata[] multiscales;
 		try {
 			final JsonElement base = n5.getAttribute(node.getPath(), "multiscales", JsonElement.class);
 			multiscales = gson.fromJson(base, OmeNgffMultiScaleMetadata[].class);
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			return Optional.empty();
 		}
 
@@ -68,14 +65,14 @@ public class OmeNgffMetadataParser implements N5MetadataParser<OmeNgffMetadata>,
 			return Optional.empty();
 
 		/*
-		 * Need to replace all children with new children with the 
-		 * metadata from this  
+		 * Need to replace all children with new children with the
+		 * metadata from this object
 		 */
-		for ( OmeNgffMultiScaleMetadata ms : multiscales ) {
+		for ( final OmeNgffMultiScaleMetadata ms : multiscales ) {
 			ms.path = node.getPath();
-			String[] paths = ms.getPaths();
-			DatasetAttributes[] attrs = new DatasetAttributes[ ms.getPaths().length ];
-			N5DatasetMetadata[] dsetMeta = new N5DatasetMetadata[ paths.length ];
+			final String[] paths = ms.getPaths();
+			final DatasetAttributes[] attrs = new DatasetAttributes[ ms.getPaths().length ];
+			final N5DatasetMetadata[] dsetMeta = new N5DatasetMetadata[ paths.length ];
 			for( int i = 0; i < paths.length; i++ ) {
 				dsetMeta[ i ] = ((N5DatasetMetadata)scaleLevelNodes.get( MetadataUtils.canonicalPath( node, paths[ i ] ) ).getMetadata());
 				attrs[ i ] = dsetMeta[ i ].getAttributes();
@@ -91,7 +88,7 @@ public class OmeNgffMetadataParser implements N5MetadataParser<OmeNgffMetadata>,
 	}
 
 	@Override
-	public void writeMetadata(OmeNgffMetadata t, N5Writer n5, String path) throws Exception {
+	public void writeMetadata(final OmeNgffMetadata t, final N5Writer n5, final String path) throws Exception {
 
 		final OmeNgffMultiScaleMetadata[] ms = t.multiscales;
 		n5.setAttribute(path, "multiscales", ms);
