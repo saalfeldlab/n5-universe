6e5826e78e49f867569749aee5aa942b9926942c
John Bogovic <bogovicj@janelia.hhmi.org>
2024-05-29 14:36:07 -0400

    fix(N5MetadataParser): simplify parseDatasetAttributes
    * style: code formatting

--- src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/N5MetadataParser.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/N5MetadataParser.java
@@ -27,59 +27,47 @@ package org.janelia.saalfeldlab.n5.universe.metadata;
 
 import java.util.Optional;
 import java.util.function.BiFunction;
-import org.janelia.saalfeldlab.n5.DataType;
+
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
-import org.janelia.saalfeldlab.n5.N5Exception;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.universe.N5TreeNode;
 
 /**
  * Interface for reading metadata from N5 containers.
- * 
+ *
  * @author Caleb Hulbert
  * @author John Bogovic
  */
 public interface N5MetadataParser<T extends N5Metadata> extends BiFunction<N5Reader, N5TreeNode, Optional<T>> {
 
-  static DatasetAttributes parseDatasetAttributes(N5Reader n5, N5TreeNode node) {
+	static DatasetAttributes parseDatasetAttributes(N5Reader n5, N5TreeNode node) {
 
-	try {
-	  final int[] blockSize = n5.getAttribute(node.getPath(), "blockSize", int[].class);
-	  final long[] dimensions = n5.getAttribute(node.getPath(), "blockSize", long[].class);
-	  final String dataTypeString = n5.getAttribute(node.getPath(), "blockSize", String.class);
-	  if (dataTypeString == null)
-		return null;
-	  final DataType dataType = DataType.fromString(dataTypeString);
-
-	  if (dimensions != null && dataType != null)
-		return new DatasetAttributes(dimensions, blockSize, dataType, null);
-	} catch (N5Exception e) {
+		return n5.getDatasetAttributes(node.getPath());
 	}
-	return null;
-  }
 
-  /**
-   * Called by the {@link org.janelia.saalfeldlab.n5.universe.N5DatasetDiscoverer}
-   * while discovering the N5 tree and filling the metadata for datasets or groups.
-   * <p>
-   * The metadata parsing is done in the bottom-up fashion, so the children of the given {@code node}
-   * have already been processed and should already contain valid metadata (if any).
-   *
-   * @param n5   the reader
-   * @param node list of tree nodes
-   * @return the metadata
-   */
-  Optional<T> parseMetadata(final N5Reader n5, final N5TreeNode node);
+	/**
+	 * Called by the {@link org.janelia.saalfeldlab.n5.universe.N5DatasetDiscoverer} while discovering the N5 tree and filling the metadata for datasets or groups.
+	 * <p>
+	 * The metadata parsing is done in the bottom-up fashion, so the children of the given {@code node} have already been processed and should already contain valid metadata (if any).
+	 *
+	 * @param n5
+	 *            the reader
+	 * @param node
+	 *            list of tree nodes
+	 * @return the metadata
+	 */
+	Optional<T> parseMetadata(final N5Reader n5, final N5TreeNode node);
 
-  default Optional<T> parseMetadata(final N5Reader n5, final String dataset) {
+	default Optional<T> parseMetadata(final N5Reader n5, final String dataset) {
 
-	if (!n5.exists(dataset))
-	  return Optional.empty();
-	return parseMetadata(n5, new N5TreeNode(dataset));
-  }
+		if (!n5.exists(dataset))
+			return Optional.empty();
+		return parseMetadata(n5, new N5TreeNode(dataset));
+	}
 
-  @Override default Optional<T> apply(N5Reader n5Reader, N5TreeNode n5TreeNode) {
+	@Override
+	default Optional<T> apply(N5Reader n5Reader, N5TreeNode n5TreeNode) {
 
-	return parseMetadata(n5Reader, n5TreeNode);
-  }
+		return parseMetadata(n5Reader, n5TreeNode);
+	}
 }
