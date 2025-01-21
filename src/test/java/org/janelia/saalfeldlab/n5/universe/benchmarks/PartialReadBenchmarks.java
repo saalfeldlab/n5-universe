5e3e9c51ce75cb5e6c2de1b9672c9c713f7250f7
John Bogovic <bogovicj@janelia.hhmi.org>
2025-01-21 16:06:12 -0500

    benchmark: improve PartialReadBenchmarks
    * compare partial to complete reads
    * partial reads start at random offsets

--- src/test/java/org/janelia/saalfeldlab/n5/universe/benchmarks/PartialReadBenchmarks.java
+++ src/test/java/org/janelia/saalfeldlab/n5/universe/benchmarks/PartialReadBenchmarks.java
@@ -9,7 +9,9 @@ import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.janelia.saalfeldlab.n5.FileSystemKeyValueAccess;
 import org.janelia.saalfeldlab.n5.KeyValueAccess;
 import org.janelia.saalfeldlab.n5.LockedChannel;
@@ -26,14 +28,15 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 @State(Scope.Benchmark)
-@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MICROSECONDS)
-@Measurement(iterations = 50, time = 100, timeUnit = TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MICROSECONDS)
+@Measurement(iterations = 50, time = 1000, timeUnit = TimeUnit.MICROSECONDS)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Fork(1)
@@ -43,19 +46,26 @@ public class PartialReadBenchmarks {
 	@Param(value = {"10000000"})
 	protected int objectSizeBytes;
 
+	@Param(value = {"1000"})
+	protected int readSizeBytes;
+
 	@Param(value = {"10", "50", "100"})
 	protected int numSubReads;
+
+	@Param(value = {"partial", "complete"})
+	protected String mode;
 
 	protected String baseDir;
 	protected KeyValueAccess kva;
 	protected Random random;
+
+	protected int[] startPositions;
 
 	public PartialReadBenchmarks() {}
 
 	@TearDown(Level.Trial)
 	public void teardown() {
 
-		System.out.println("teardown");
 		for (final int sz : sizes()) {
 			new File(baseDir, "" + sz).delete();
 			new File(baseDir).delete();
@@ -65,7 +75,7 @@ public class PartialReadBenchmarks {
 	@Setup(Level.Trial)
 	public void setup() {
 
-		random = new Random();
+		random = new Random(1);
 		kva = new FileSystemKeyValueAccess(FileSystems.getDefault());
 
 		File tmpFile;
@@ -75,6 +85,11 @@ public class PartialReadBenchmarks {
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}
+
+		startPositions = IntStream.generate(() -> {
+
+			return random.nextInt(objectSizeBytes - readSizeBytes);
+		}).limit(numSubReads).toArray();
 
 		for (final int sz : sizes()) {
 			String path;
@@ -102,29 +117,42 @@ public class PartialReadBenchmarks {
 		}
 	}
 
-	private void read(String path, int startByte, int numBytes) {
+	private byte[] read(String path, int startByte, int numBytes) throws IOException {
 
 		try (final LockedChannel ch = kva.lockForReading(path, startByte, numBytes)) {
 			final InputStream is = ch.newInputStream();
 			final byte[] data = new byte[numBytes];
 			is.read(data);
 			is.close(); // not strictly needed
-		} catch (final IOException e) {
-			e.printStackTrace();
+			return data;
+		}
+	}
+
+	private byte[] readComplete(String path) throws IOException {
+
+		try (final ByteArrayOutputStream result = new ByteArrayOutputStream()) {
+
+			try (final LockedChannel ch = kva.lockForReading(path)) {
+				final InputStream is = ch.newInputStream();
+				byte[] buffer = new byte[1024];
+				for (int length; (length = is.read(buffer)) != -1;) {
+					result.write(buffer, 0, length);
+				}
+				return result.toByteArray();
+			}
 		}
 	}
 
 	@Benchmark
-	public void run() throws IOException {
+	public void run(Blackhole blackhole) throws IOException {
 
 		final String path = new File(baseDir, "" + objectSizeBytes).getCanonicalPath();
-
-		// read
-		final int numBytesToRead = objectSizeBytes / numSubReads;
-		int start = 0;
-		for (int i = 0; i < numSubReads; i++) {
-			read(path, start, numBytesToRead);
-			start += numBytesToRead;
+		if (mode.equals("partial")) {
+			for (int i = 0; i < numSubReads; i++) {
+				blackhole.consume(read(path, startPositions[i], readSizeBytes));
+			}
+		} else {
+			blackhole.consume(readComplete(path));
 		}
 	}
 
