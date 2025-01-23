2c342f83ac02888d59126126c970678ca68be4d5
John Bogovic <bogovicj@janelia.hhmi.org>
2025-01-23 14:41:57 -0500

    perf: add OmeNgff v0.5 metadata parser to default list

--- src/main/java/org/janelia/saalfeldlab/n5/universe/N5DatasetDiscoverer.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/N5DatasetDiscoverer.java
@@ -52,6 +52,7 @@ import org.janelia.saalfeldlab.n5.universe.metadata.N5SingleScaleMetadataParser;
 import org.janelia.saalfeldlab.n5.universe.metadata.N5ViewerMultiscaleMetadataParser;
 import org.janelia.saalfeldlab.n5.universe.metadata.canonical.CanonicalMetadataParser;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.OmeNgffMetadataParser;
+import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v05.OmeNgffV05MetadataParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -88,6 +89,7 @@ public class N5DatasetDiscoverer {
 	};
 
 	public static final N5MetadataParser<?>[] DEFAULT_GROUP_PARSERS = new N5MetadataParser<?>[]{
+			new OmeNgffV05MetadataParser(),
 			new OmeNgffMetadataParser(),
 			new N5CosemMultiScaleMetadata.CosemMultiScaleParser(),
 			new N5ViewerMultiscaleMetadataParser(),
