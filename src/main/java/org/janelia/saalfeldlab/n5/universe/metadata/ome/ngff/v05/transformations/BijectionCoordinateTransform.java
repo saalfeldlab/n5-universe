5175db2d279659fd93146e2a016d030d4d72a2f7
John Bogovic <bogovicj@janelia.hhmi.org>
2025-02-11 20:45:42 -0500

    feat: annotation serialization for bijection transform

--- src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/v05/transformations/BijectionCoordinateTransform.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/v05/transformations/BijectionCoordinateTransform.java
@@ -1,42 +1,52 @@
 package org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v05.transformations;
 
 import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.universe.serialization.NameConfig;
 
 import net.imglib2.realtransform.ExplicitInvertibleRealTransform;
 
+@NameConfig.Name("bijection")
 public class BijectionCoordinateTransform extends AbstractCoordinateTransform<ExplicitInvertibleRealTransform> 
 	implements InvertibleCoordinateTransform<ExplicitInvertibleRealTransform> {
 
-	private final RealCoordinateTransform<?> forward;
+	public static final String TYPE = "bijection";
 
-	private final RealCoordinateTransform<?> inverse;
+	@NameConfig.Parameter
+	private final CoordinateTransform<?> forward;
 
-	public BijectionCoordinateTransform( final String name, final String inputSpace, final String outputSpace,
-		final RealCoordinateTransform<?> forward, final RealCoordinateTransform<?> inverse) {
-		super("bijection", name, inputSpace, outputSpace );
+	@NameConfig.Parameter
+	private final CoordinateTransform<?> inverse;
+
+	protected BijectionCoordinateTransform() {
+		// for serialization
+		super(TYPE);
+		forward = null;
+		inverse = null;
+	}
+
+	public BijectionCoordinateTransform(final String name, final String inputSpace, final String outputSpace,
+			final CoordinateTransform<?> forward, final CoordinateTransform<?> inverse) {
+		super(TYPE, name, inputSpace, outputSpace);
 		this.forward = forward;
 		this.inverse = inverse;
 	}
 
-	public RealCoordinateTransform<?> getForward() {
+	public CoordinateTransform<?> getForward() {
 		return forward;
 	}
 
-	public RealCoordinateTransform<?> getInverse() {
+	public CoordinateTransform<?> getInverse() {
 		return inverse;
 	}
 	
 	@Override
-	public ExplicitInvertibleRealTransform getTransform( final N5Reader n5 )
-	{
+	public ExplicitInvertibleRealTransform getTransform(final N5Reader n5) {
 		return new ExplicitInvertibleRealTransform(forward.getTransform(n5), inverse.getTransform(n5));
 	}
 
 	@Override
-	public ExplicitInvertibleRealTransform getTransform()
-	{
+	public ExplicitInvertibleRealTransform getTransform() {
 		return new ExplicitInvertibleRealTransform(forward.getTransform(), inverse.getTransform());
 	}
-
 
 }
