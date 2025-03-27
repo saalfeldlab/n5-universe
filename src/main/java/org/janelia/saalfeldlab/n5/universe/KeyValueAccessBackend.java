5a126a2acc10836e424f03b35c7fb9f506a82769
Caleb Hulbert <cmhulbert@gmail.com>
2025-03-27 16:17:19 -0400

    feat: add support for HTTPKeyValueAccess

--- src/main/java/org/janelia/saalfeldlab/n5/universe/KeyValueAccessBackend.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/KeyValueAccessBackend.java
@@ -4,7 +4,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import org.janelia.saalfeldlab.googlecloud.GoogleCloudStorageURI;
 import org.janelia.saalfeldlab.googlecloud.GoogleCloudUtils;
 import org.janelia.saalfeldlab.n5.FileSystemKeyValueAccess;
+import org.janelia.saalfeldlab.n5.HttpKeyValueAccess;
 import org.janelia.saalfeldlab.n5.KeyValueAccess;
+import org.janelia.saalfeldlab.n5.N5Exception;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.N5Writer;
 import org.janelia.saalfeldlab.n5.googlecloud.GoogleCloudStorageKeyValueAccess;
@@ -16,7 +18,6 @@ import java.net.URI;
 import java.nio.file.FileSystems;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 
 /**
  * Enum to discover and provide {@link KeyValueAccess} for {@link N5Reader}s and {@link N5Writer}s.
@@ -40,6 +41,11 @@ public enum KeyValueAccessBackend implements Predicate<URI>, BiFunction<URI, N5F
 		return hasScheme && AmazonS3Utils.S3_SCHEME.asPredicate().test(scheme)
 				|| uri.getHost() != null && hasScheme && N5Factory.HTTPS_SCHEME.asPredicate().test(scheme);
 	}, KeyValueAccessBackend::newAmazonS3KeyValueAccess),
+	HTTP(uri -> {
+		final String scheme = uri.getScheme();
+		final boolean hasScheme = scheme != null;
+		return hasScheme && N5Factory.HTTPS_SCHEME.asPredicate().test(scheme);
+	}, KeyValueAccessBackend::newHttpKeyValueAccess),
 	FILE(uri -> {
 		final String scheme = uri.getScheme();
 		final boolean hasScheme = scheme != null;
@@ -57,9 +63,7 @@ public enum KeyValueAccessBackend implements Predicate<URI>, BiFunction<URI, N5F
 
 	@Override public KeyValueAccess apply(final URI uri, final N5Factory factory) {
 
-		if (test(uri))
-			return backendGenerator.apply(uri, factory);
-		return null;
+		return backendGenerator.apply(uri, factory);
 	}
 
 	@Override public boolean test(URI uri) {
@@ -87,11 +91,21 @@ public enum KeyValueAccessBackend implements Predicate<URI>, BiFunction<URI, N5F
 		 * backend take into account reasonable defaults when possible.
 		 * Here we test from most to least restrictive.
 		 * See the Javadoc for more details.  */
+		N5Exception.N5IOException testPassedButThrown = null;
 		for (final KeyValueAccessBackend backend : KeyValueAccessBackend.values()) {
-			final KeyValueAccess kva = backend.apply(uri, factory);
-			if (kva != null)
-				return kva;
+			if (backend.test(uri)) {
+				try {
+					return backend.apply(uri, factory);
+				} catch (final Exception e) {
+					String reason = String.format("Inferred Backend was %s but could not create KeyValueAccess", backend);
+					testPassedButThrown = new N5Exception.N5IOException(reason, e);
+				}
+			}
 		}
+		if (testPassedButThrown != null) {
+			throw testPassedButThrown;
+		}
+
 		return null;
 	}
 
@@ -112,5 +126,10 @@ public enum KeyValueAccessBackend implements Predicate<URI>, BiFunction<URI, N5F
 	private static FileSystemKeyValueAccess newFileSystemKeyValueAccess(final URI uri, final N5Factory factory) {
 
 		return new FileSystemKeyValueAccess(FileSystems.getDefault());
+	}
+
+	private static HttpKeyValueAccess newHttpKeyValueAccess(final URI uri, final N5Factory factory) {
+
+		return new HttpKeyValueAccess();
 	}
 }
