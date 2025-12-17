161f3983670284f639b6175dd6766cd7ed589e5d
John Bogovic <bogovicj@janelia.hhmi.org>
2025-12-17 15:57:05 -0500

    feat: add axis permutation helpers to OmeNgffMetadata

--- src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/v04/OmeNgffMetadata.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/v04/OmeNgffMetadata.java
@@ -1,12 +1,17 @@
 package org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04;
 
+import org.janelia.saalfeldlab.n5.universe.metadata.N5Metadata;
 import org.janelia.saalfeldlab.n5.universe.metadata.SpatialMultiscaleMetadata;
 import org.janelia.saalfeldlab.n5.universe.metadata.axes.Axis;
+import org.janelia.saalfeldlab.n5.universe.metadata.axes.AxisMetadata;
 import org.janelia.saalfeldlab.n5.universe.metadata.axes.AxisUtils;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.OmeNgffMultiScaleMetadata.OmeNgffDataset;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.coordinateTransformations.CoordinateTransformation;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.coordinateTransformations.ScaleCoordinateTransformation;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v04.coordinateTransformations.TranslationCoordinateTransformation;
+
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.view.Views;
 
 public class OmeNgffMetadata extends SpatialMultiscaleMetadata<NgffSingleScaleAxesMetadata>
 {
@@ -70,6 +75,51 @@ public class OmeNgffMetadata extends SpatialMultiscaleMetadata<NgffSingleScaleAx
 				datasets, null, cts, null);
 
 		return new OmeNgffMetadata("", new OmeNgffMultiScaleMetadata[]{ ms });
+	}
+
+	public static <T, M extends AxisMetadata & N5Metadata> RandomAccessibleInterval<T> permuteForNgff(
+			final RandomAccessibleInterval<T> img,
+			final M meta) {
+
+		final int[] p = findNgffPermutation(meta.getAxisLabels());
+		AxisUtils.fillPermutation(p);
+
+		// TODO under what conditions can I return the image directly?
+		RandomAccessibleInterval<T> imgTmp = img;
+		while (imgTmp.numDimensions() < 5)
+			imgTmp = Views.addDimension(imgTmp, 0, 0);
+
+		if (AxisUtils.isIdentityPermutation(p))
+			return imgTmp;
+
+		return AxisUtils.permute(imgTmp, AxisUtils.invertPermutation(p));
+	}
+
+	/**
+	 * Finds and returns a permutation p such that source[p[i]] equals xyczt
+	 *
+	 * @param axisLabels
+	 *            the axis labels
+	 * @return the permutation array
+	 */
+	public static int[] findNgffPermutation(final String[] axisLabels) {
+
+		final int[] p = new int[5];
+		p[0] = indexOf(axisLabels, "x");
+		p[1] = indexOf(axisLabels, "y");
+		p[2] = indexOf(axisLabels, "z");
+		p[3] = indexOf(axisLabels, "c");
+		p[4] = indexOf(axisLabels, "t");
+		return p;
+	}
+
+	private static final <T> int indexOf(final T[] arr, final T tgt) {
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals(tgt))
+				return i;
+		}
+		return -1;
 	}
 
 }
