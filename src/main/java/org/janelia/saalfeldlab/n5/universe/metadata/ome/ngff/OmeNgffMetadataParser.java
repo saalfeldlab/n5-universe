cc5ab4e1f3e07acc37ec35bc7ab9ac7fdc062d76
John Bogovic <bogovicj@janelia.hhmi.org>
2026-05-06 14:15:21 -0400

    fix: OmeNgffMetadataParser writes zarr3 dimension_names

--- src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/OmeNgffMetadataParser.java
+++ src/main/java/org/janelia/saalfeldlab/n5/universe/metadata/ome/ngff/OmeNgffMetadataParser.java
@@ -1,16 +1,18 @@
 package org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
-import org.janelia.saalfeldlab.n5.N5KeyValueReader;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.N5Writer;
 import org.janelia.saalfeldlab.n5.universe.N5TreeNode;
 import org.janelia.saalfeldlab.n5.universe.metadata.MetadataUtils;
-import org.janelia.saalfeldlab.n5.universe.metadata.N5DatasetMetadata;
 import org.janelia.saalfeldlab.n5.universe.metadata.N5MetadataParser;
 import org.janelia.saalfeldlab.n5.universe.metadata.N5MetadataWriter;
 import org.janelia.saalfeldlab.n5.universe.metadata.axes.Axis;
@@ -20,7 +22,7 @@ import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.coordinateTransform
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.coordinateTransformations.CoordinateTransformationAdapter;
 import org.janelia.saalfeldlab.n5.universe.metadata.ome.ngff.v03.OmeNgffV03MetadataProcessor;
 import org.janelia.saalfeldlab.n5.zarr.ZarrKeyValueReader;
-import org.janelia.saalfeldlab.n5.zarr.ZarrKeyValueWriter;
+import org.janelia.saalfeldlab.n5.zarr.v3.ZarrV3DatasetAttributes;
 import org.janelia.saalfeldlab.n5.zarr.v3.ZarrV3KeyValueReader;
 import org.janelia.saalfeldlab.n5.zarr.v3.ZarrV3KeyValueWriter;
 
@@ -163,9 +165,29 @@ public class OmeNgffMetadataParser implements N5MetadataParser<OmeNgffMetadata>,
 		if( t.multiscales[0].version.equals("0.5")) {
 			n5.setAttribute(groupPath, OME + "/version", "0.5");
 			n5.setAttribute(groupPath, OMEMS, jsonElem);
+			writeZarr3DimensionNames(n5, groupPath, ms);
 		}
 		else
 			n5.setAttribute(groupPath, MS, jsonElem);
+	}
+
+	private void writeZarr3DimensionNames(N5Writer n5, final String groupPath, OmeNgffMultiScaleMetadata[] ms) {
+
+		if (!(n5 instanceof ZarrV3KeyValueWriter)) {
+			return;
+		}
+
+		final ZarrV3KeyValueWriter zarr3 = (ZarrV3KeyValueWriter)n5;
+		final List<String> axisNames = Stream.of(ms[0].getAxes()).map(Axis::getName)
+				.collect(Collectors.toList());
+
+		if (reverse)
+			Collections.reverse(axisNames);
+
+		for (OmeNgffDataset dataset : ms[0].getDatasets()) {
+			final String path = groupPath + "/" + dataset.path;
+			zarr3.setRawAttribute(path, ZarrV3DatasetAttributes.DIMENSION_NAMES_KEY, axisNames);
+		}
 	}
 
 }
